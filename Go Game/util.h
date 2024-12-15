418ef31fbf6e01c94aa6e1c8f322fe59270b7ea3
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-12-15 15:06:45 +0500

    Summary

---
+++ Go Game/util.h
@@ -0,0 +1,16 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <iostream>
+#include <cstdlib>
+
+
+void _assertNotNull(void * obj, std::ostream &out = std::cerr){
+        if (obj == NULL){
+            out << "Object "<<obj<<" is NULL."<< std::endl;
+
+            exit(666); // todo: change exit to an exception probably
+        }
+    }
+
+#endif // UTIL_H
